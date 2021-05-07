0151bf2d95b9779fc0f3a27c34a69050d481e464
Parthbharti-Goswami <83647381+Parthbharti-Goswami@users.noreply.github.com>
2021-05-07 21:44:45 +0530

    Done

--- IOS_Segment/ViewController.swift
+++ IOS_Segment/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cl_seg: UISegmentedControl!
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    
+    @IBOutlet weak var years_lbl: UILabel!
+    @IBOutlet weak var gen_lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gen()
     }
-
-
+    
+    
+    @IBAction func gen_act(_ sender: Any) {
+        
+        gen()
+    }
+    
+    func gen(){
+        
+        if cl_seg.selectedSegmentIndex == 0{
+            
+            img.image = #imageLiteral(resourceName: "Gen-1")
+            years_lbl.text = "1999-2008"
+            gen_lbl.text = "First Generation Hayabusa"
+            
+        }else if cl_seg.selectedSegmentIndex == 1{
+            
+            img.image = #imageLiteral(resourceName: "Gen-2")
+            years_lbl.text = "2008-2020"
+            gen_lbl.text = "Second Generation Hayabusa"
+            
+        }else if cl_seg.selectedSegmentIndex == 2{
+            
+            img.image = #imageLiteral(resourceName: "Gen-3")
+            years_lbl.text = "2021-Present"
+            gen_lbl.text = "Third Generation Hayabusa"
+            
+        }
+        
+        
+        
+    }
+    
+    
 }
 
